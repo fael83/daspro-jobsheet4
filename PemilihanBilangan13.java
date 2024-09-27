bcd94c18941b32d6db2fae635c6becf33ca5108e
fael83 <rafaellokajaya@gmail.com>
2024-09-27 09:43:49 +0700

    Simpan kode java ke github

--- PemilihanBilangan13.java
+++ PemilihanBilangan13.java
@@ -2,15 +2,21 @@ import java.util.Scanner;
 
 public class PemilihanBilangan13 {
     public static void main(String[] args) {
+        String hasil;
         Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan sebuah angka");
+        System.out.print("Masukkan sebuah angka: ");
         int angka = input.nextInt();
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + " termasuk bilangan genap" );
-        } else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        
+        // if (angka % 2 == 0) {
+        //     System.out.println("Angka " + angka + " termasuk bilangan genap" );
+        // } else {
+        //     System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        // }
 
+        // operator ternary
+        hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+        System.out.println("Angka " + angka  + " " + hasil);
+        
     }
 } 
     
